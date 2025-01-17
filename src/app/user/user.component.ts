0f1e5c9c3bd8cf06042791087fc0c08de29c4940
Michelle Sarmiento <michellechicsarmiento@yahoo.com>
2025-01-17 08:53:19 +0100

    replacing signal with input

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component,computed,signal } from '@angular/core';
+import { Component,computed,Input,signal } from '@angular/core';
 
 import {DUMMY_USERS} from '../dummy-users'
 
@@ -11,17 +11,22 @@ const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]); /*initializing signal*/
+  // selectedUser = signal(DUMMY_USERS[randomIndex]); /*initializing signal*/
 
-  imgPath= computed(()=>'assets/users/'+ this.selectedUser().avatar)
+  // imgPath= computed(()=>'assets/users/'+ this.selectedUser().avatar)
 
-  // get imgPath(){
-  //   return 'assets/users/'+ this.selectedUser.avatar
-  // }
+
+
+  @Input() photo!: string;
+  @Input() name!: string;
+
+  get imgPath(){
+    return 'assets/users/'+ this.photo;
+  }
 
   onSelectUser(){
-    const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);/*updating signal value, you then call it as a function*/
+    // const randomIndex = Math.floor(Math.random()* DUMMY_USERS.length);
+    // this.selectedUser.set(DUMMY_USERS[randomIndex]);/*updating signal value, you then call it as a function*/
 
   }
 
