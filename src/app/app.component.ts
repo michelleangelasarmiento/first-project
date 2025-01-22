27f8f2e693817f4343d3c5c6b47b261c3aa35075
Michelle Sarmiento <michellechicsarmiento@yahoo.com>
2025-01-22 10:18:48 +0100

    temporary solutuon, if-else not working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component ,computed} from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
 import { DetailsComponent } from './details/details.component';
+
 
 @Component({
     selector: 'app-root',
@@ -13,11 +14,17 @@ import { DetailsComponent } from './details/details.component';
 })
 export class AppComponent {
     users = DUMMY_USERS;
-    selectedUserId?: string;
+    selectedUserId ='u1'
 
     get selectedUser() {
-        return this.users.find((user) => user.id === this.selectedUserId);
+        return this.users.find(user => user.id === this.selectedUserId)!;
     }
+
+    // selectedUserTasks = computed(() => {
+    //     return this.users.filter((task) => {
+    //         task.id === this.selectedUserId;
+    //     })
+    // })
 
     onSelectUser(id: string) {
         this.selectedUserId = id;
